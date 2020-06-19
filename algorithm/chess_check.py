89a1abddd7e2d5308f498a13aa6a60d518adfbfa
AuthorityWu <AuthorityWu@Outlook.com>
2020-06-19 11:56:42 +0800

    create

---
+++ algorithm/chess_check.py
@@ -0,0 +1,51 @@
+
+
+class ChessCheck():
+
+    def __init__(self):
+        pass
+
+    def code_to_martix(self, code):
+        """
+        棋局状态码转棋局矩阵
+        parm: (棋局状态码)
+        return: 棋局矩阵
+        """
+
+        pass
+
+    def matrix_to_code(self, matrix):
+        """
+        棋局矩阵转棋局状态码
+        parm: (棋局矩阵)
+        return: 棋局状态码
+        """
+
+        pass
+
+
+    def is_legal(self, matrix, move, color):
+        """
+        判断下棋动作是否合法 
+        parm: (棋局状态, 下棋动作, 下棋方)
+        retrun: True/False
+        """
+
+        # 判断是否越界
+
+        # 判断该棋子的下棋动作是否符合一步内可达
+
+        # 判断能否吃子
+        pass
+
+    def is_over(self, code):
+        """
+        判断当局是否结束 
+        parm: (棋局状态码)
+        return: 当前棋局状态(红胜：-１；未结束：0；黑胜：１)
+        """
+        # 判断是否有一方将领消失
+
+        pass
+        
+
---
+++ algorithm/chess_rule.py
@@ -0,0 +1,69 @@
+
+
+class ChessRule():
+    def __init__(self):
+        pass
+    
+    """
+    各个棋子的一步动作的合法性校验
+    parm: (棋局矩阵, 下棋动作)
+    return: True/False
+    """
+    # 红
+    @staticmethod
+    def hongche(matrix, move):
+        pass
+
+    @staticmethod
+    def hongma(matrix, move):
+        pass
+
+    @staticmethod
+    def hongxiang(matrix, move):
+        pass
+
+    @staticmethod
+    def hongshi(matrix, move):
+        pass
+    
+    @staticmethod
+    def hongshuai(matrix, move):
+        pass
+
+    @staticmethod
+    def hongpao(matrix, move):
+        pass
+
+    @staticmethod
+    def hongbing(matrix, move):
+        pass
+    
+
+    # 黑
+    @staticmethod
+    def heiche(matrix, move):
+        pass
+
+    @staticmethod
+    def heima(matrix, move):
+        pass
+    
+    @staticmethod
+    def heixiang(matrix, move):
+        pass
+
+    @staticmethod
+    def heishi(matrix, move):
+        pass
+
+    @staticmethod
+    def heijiang(matrix, move):
+        pass
+
+    @staticmethod
+    def heipao(matrix, move):
+        pass
+
+    @staticmethod
+    def heizu(matrix, move):
+        pass
