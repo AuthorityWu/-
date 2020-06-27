c56eb9a66fe7d576a1ff7d05db77bd26e198dd3c
blues <1783357272@qq.com>
2020-06-28 00:18:27 +0800

    改完bug以后的前端

--- view/chessView/chess.js
+++ view/chessView/chess.js
@@ -5,13 +5,21 @@
 			var round=0;
 			var selected=null;
 			var target=99;
-			
+			var train=0;
 			for(var i=0;i<9;i++)
 			{
 				matrix[i]=new Array(10);
 				for(var j=0;j<10;j++){
 					matrix[i][j]=-1;
 				}
+			}
+			function Train(){
+				train=1;
+				Start("begin");
+			}
+			function choose_black(){
+				Start("begin");
+				Ask_for_black("-1");
 			}
 			function start(){
 				var flag=0;
@@ -38,43 +46,57 @@
 				var img=0;
 				selected=null;
 				target=99;
+				for(var i=0;i<9;i++)
+			{
+				matrix[i]=new Array(10);
+				for(var j=0;j<10;j++){
+					matrix[i][j]=-1;
+				}
+			}
 				while(flag!=64){
-					var list=(string.substring(flag,flag+1));
-					var line=(string.substring(flag+1,flag+2));
-					//console.log(line+list);
-					matrix[list][line]=img;
-					//console.log(line+list);
-					$("#"+list+line).append("<img class='img_chess' src='img/drawable-xhdpi/"+Chesspiece[img]+".png'>")
-					//$("#"+line+list).css("background","url(img/drawable-xhdpi/"+Chesspiece[img]+".png)");
+					if(Chess.substring(flag,flag+2)!=99){
+						var list=(string.substring(flag,flag+1));
+						var line=(string.substring(flag+1,flag+2));
+						//console.log(line+list);
+						matrix[list][line]=img;
+						//console.log(line+list);
+						$("#"+list+line).append("<img class='img_chess' src='img/drawable-xhdpi/"+Chesspiece[img]+".png'>")
+						//$("#"+line+list).css("background","url(img/drawable-xhdpi/"+Chesspiece[img]+".png)");
+											
+					}
 					img++;
 					flag+=2;
+					
 				}
+				console.log(matrix)
 			}
 			function Start(begin){
 				var flag=0;
 				var img=0;
 				$("#"+begin).remove();
-				while(flag!=64&&Chess.substring(flag,flag+2)!=99){
-					var list=(Chess.substring(flag,flag+1));
-					var line=(Chess.substring(flag+1,flag+2));
-					//console.log(line+list);
-					matrix[list][line]=img;
-					//console.log(line+list);
-					$("#"+list+line).append("<img class='img_chess' src='img/drawable-xhdpi/"+Chesspiece[img]+".png'>")
-					//$("#"+line+list).css("background","url(img/drawable-xhdpi/"+Chesspiece[img]+".png)");
+				while(flag!=64){
+					if(Chess.substring(flag,flag+2)!=99){
+						var list=(Chess.substring(flag,flag+1));
+						var line=(Chess.substring(flag+1,flag+2));
+						//console.log(line+list);
+						matrix[list][line]=img;
+						//console.log(line+list);
+						$("#"+list+line).append("<img class='img_chess' src='img/drawable-xhdpi/"+Chesspiece[img]+".png'>")
+						//$("#"+line+list).css("background","url(img/drawable-xhdpi/"+Chesspiece[img]+".png)");
+					}
 					img++;
 					flag+=2;
 				}
+				
 			
 			}
 			function restart(){
-				$(".end").remove();
-				$(".img_chess").remove();
+				window.location.reload()
 				Chess=Newchess;
 				round=0;
 				selected=null;
 				target=99;
-				start();
+				//start();
 			}
 			function change(id){
 				
@@ -98,7 +120,7 @@
 					success: function(msg){
 					//console.log("link")
 					if(msg!=''){
-						console.log(msg.flag) //将返回的json数据进行解析，并赋给data
+						console.log(msg) //将返回的json数据进行解析，并赋给data
 					}
 					//console.log(msg); //控制台输出
 					//console.log(Chess);
@@ -107,6 +129,7 @@
 					   // 对msg数据进行分析
 					if(msg.flag == true) {
 					   console.log("ok")
+					   Chess=state;
 					   f=true;
 					} 
 					else{
@@ -121,7 +144,7 @@
 					  }
 					  });
 					  console.log("flag:"+f)
-					  Chess=state;
+					  
 					  return f;
 				/*$.getJSON('http://127.0.0.1:5000/Login?name=ll&pw=123&callback=?', function(data){
 					console.log(data);
@@ -145,23 +168,24 @@
 					  }
 					  });
 			}
-			function Ask_for_black(){
+			function Ask_for_black(color){
 				var state=null;
 				var move=null;
 				//console.log("black1")
 				 $.ajax({
 					  type: "get",
 					  url: "http://120.79.195.191:80/getMove",
-					  data: {code:Chess,color:"1"}, 
+					  data: {code:Chess,color:color}, 
 					  dataType: "json", //回调函数接收数据的数据格式	
 						async : false,
 					  success: function(msg){
-						console.log("black")
+						//console.log("black")
 					   if(msg!=''){
 							state=msg.new_code;
 							move=msg.move;
-							console.log("新+"+state);
-							console.log("新+"+move);
+							console.log(msg)
+							//console.log("新+"+state);
+							//console.log("新+"+move);
 					   }
 					  },
 					  error:function(msg){
@@ -170,17 +194,28 @@
 					  });
 				if(state!=null&&move!=null){
 					//console.log("black:"+state)
-					Black_move(state)
+					if(round==2)
+						Black_move(state);
+					else if(round==0)
+						Red_move(state);
 				}
 			}
 			function end(){
 				if(Chess.substring(8,10)==99){
 						$("#Bigcontainer").append("<div class='end' style='width:100%;height:100%;position:absolute;left:0px;background:url(img/drawable-xhdpi/结束画面_黑.png);background-size:100% 100%' onclick='Start('begin')'><img style='width:170px;height:60px;margin-top:260px;margin-left:180px'src='img/drawable-xhdpi/再来一局.png' onclick='restart()'></div>")
+						return false;
 					}
-					else if(Chess.substring(40,42)==99){
+				else if(Chess.substring(40,42)==99){
 						$("#Bigcontainer").append("<div class='end' style='width:100%;height:100%;position:absolute;left:0px;background:url(img/drawable-xhdpi/结束画面_红.png);background-size:100% 100%' onclick='Start('begin')'><img style='width:170px;height:60px;margin-top:260px;margin-left:180px'src='img/drawable-xhdpi/再来一局.png' onclick='restart()'></div>")
+						return false;
 					}
+				else return true;
 				
+			}
+			function Red_move(string){
+				BuildM(string);
+				end()
+				round=2;
 			}
 			function Black_move(string){
 				BuildM(string);
@@ -191,12 +226,11 @@
 			function Move(id){
 				//console.log('我是？？新的，要到的位置的id?'+id);
 			var img=matrix[selected.substring(0,1)][selected.substring(1,2)];
+			var temp=Chess;
 				if(Query(id,img)){
 					// 调用learn函数需要用到的变量
-					console.log("move")
+					//console.log("move")
 					var move=selected+id;
-					var state=Chess.substring(0,img*2)+id+Chess.substring(img*2+2,64);
-					
 					if(img!=-1){
 						$("#"+id).empty()
 					}
@@ -207,21 +241,24 @@
 					matrix[selected.substring(0,1)][selected.substring(1,2)]=-1
 					selected=null;
 					//console.log("黑将"+state.substring(40,42))
-					end()
-					if(round==0)
-						round=2;
-					else if(round==2)
-						round=0;
+					if(end()){
+						if(round==0)
+							round=2;
+						else if(round==2)
+							round=0;
+						//console.log("红棋移动前"+temp);
+						//console.log(move);
+						Learn(temp,move);
+						//console.log("红棋移动后"+Chess)
+						if(train==0){
+							if(round==2)
+								Ask_for_black("1");//红棋移动成功后黑棋移动
+							else if(round==0)
+								Ask_for_black("-1");
+						
+						}
+					}
 					
-					console.log(Chess);
-					console.log(move);
-					// 调用learn函数
-					Learn(Chess,move);
-					// 成功移动后，更新棋局状态
-					Chess = state;
-					console.log(round)
-					if(round==2)
-						Ask_for_black();//红棋移动成功后黑棋移动
 					//console.log(Chess);
 				}
 			}
